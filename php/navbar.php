8425c93e302e128e768261c38b8caeb1bf25c800
SaiPreetham <70648426+Saipreetham7@users.noreply.github.com>
2022-06-20 22:42:33 +0530

    Add files via upload

---
+++ php/navbar.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <style>
+    a:link {
+      color: #112B3C;
+    }
+
+    a:hover {
+      color: white;
+      text-decoration: none;
+    }
+  </style>
+</head>
+
+<body>
+  <!--Responsive Navbar-->
+  <nav>
+    <div class="container-fluid nav-wrapper #00838f nav1 darken-3 z-depth-5">
+      <a href="../index.php" class="brand-logo"><img src="https://www.thesparksfoundationsingapore.org/images/logo_small.png" style="height:70px;width:60px" /></a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li class="nav2"><a href="../index.php">Home</a></li>
+        <li class="nav2"><a href="transfermoney.php">View Customers</a></li>
+        <li class="nav2"><a href="transactionhistory.php">Transactions</a></li>
+      </ul>
+    </div>
+  </nav>
+  <!--end of navbar-->
+</body>
+
+</html>
